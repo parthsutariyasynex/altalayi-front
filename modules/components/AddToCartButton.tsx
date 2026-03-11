b06c3507ace59b5190d038eed186b119dcb50595
parthsutariyasynex <parthsutariya.synex@gmail.com>
2026-03-11 16:17:35 +0530

    fix: build errors resolved

--- modules/components/AddToCartButton.tsx
+++ modules/components/AddToCartButton.tsx
@@ -1,12 +1,27 @@
+// "use client";
+
+// import { useCart } from "@/modules/cart/hooks/useCart";
+
+// export default function AddToCartButton({ sku }: { sku: string }) {
+//   const { addToCart } = useCart();
+
+//   return (
+//     <button onClick={() => addToCart(sku, 1)}>
+//       Add To Cart
+//     </button>
+//   );
+// }
+
+
 "use client";
 
 import { useCart } from "@/modules/cart/hooks/useCart";
 
-export default function AddToCartButton({ sku }: { sku: string }) {
+export default function AddToCartButton({ sku }: { sku: string | number }) {
   const { addToCart } = useCart();
 
   return (
-    <button onClick={() => addToCart(sku, 1)}>
+    <button onClick={() => addToCart(String(sku), 1)}>
       Add To Cart
     </button>
   );
