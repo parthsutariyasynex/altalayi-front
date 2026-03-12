63af082d18f5ef3396d3eaaca756ded3eae54a93
parthsutariyasynex <parthsutariya.synex@gmail.com>
2026-03-12 19:53:12 +0530

    feat: implement login, logout, forgot password, and OTP authentication

--- app/category/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-// import Sidebar from "../../components/Sidebar";
-
-// export default function Page() {
-//   return (
-//     <div className="flex">
-//       <Sidebar  />
-//     </div>
-//   );
-// }
